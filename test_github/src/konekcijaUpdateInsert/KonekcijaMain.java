4292ed2e135cc8d3726d39038c81450431688cbb
igor.nasteski <igor.nasteski@INASTESKI.PCI.local>
2019-04-15 13:50:45 +0200

    Dodata metoda za proveru jednakosti niza unapred i unazad

--- test_github/src/konekcijaUpdateInsert/KonekcijaMain.java
+++ test_github/src/konekcijaUpdateInsert/KonekcijaMain.java
@@ -7,9 +7,30 @@ public class KonekcijaMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		MetodeJdbc.insertUpdateIliDelete_Baza();
-		System.out.println("Dodata nova linija koda za git");
+		//MetodeJdbc.insertUpdateIliDelete_Baza();
+		//System.out.println("Dodata nova linija koda za git");
 		
+		int demoNiz[] = {1,4,5,2,5,4,1}; 
+		uporediNizUnazad(demoNiz);
+		
+	}
+	
+	public static void uporediNizUnazad(int niz[]) {
+		
+		String odgovor = "";
+		int unazad = niz.length - 1;
+		for (int i=0; i<niz.length; i++) {
+			if(niz[i] != niz[unazad]) {
+				odgovor = "Nije";
+				break;
+			}
+			else {
+				odgovor = "Jeste";
+			}
+			unazad--;
+		}
+		
+		System.out.println(odgovor);
 	}
 
 }
